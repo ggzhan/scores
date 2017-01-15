93836b2696e197065c43633afc531d58a2e8a5f5
Gerald Z <geraldz@geralds-mac.local>
2017-01-15 16:18:54 +0100

    finished swa.py translation

--- Gerald Scores/SmithWaterman.swift
+++ Gerald Scores/SmithWaterman.swift
@@ -22,11 +22,18 @@ func reset() {
     max2 = 0
 }
 
+func all(v1: [Float], v2: [Float]) -> Bool {
+    for i in 0...v1.count-1 {
+        if (v1[i] != v2[i]) {return false}
+    }
+    return true
+}
+
 // notes by Alex: Seems to be faster for vectors of length 12 (Chromas). return 1.0 - cosine(v1, v2)
 func cosine_similarity(v1: [Float], v2: [Float]) -> Float {
     var v1IsZero: Bool = false
     var v1IsZeroFunc: Bool{
-        for i in v1 {
+        for i in 0...v1.count-1 {
             if (i != 0) {return false}
         }
         return true
@@ -45,19 +52,19 @@ func cosine_similarity(v1: [Float], v2: [Float]) -> Float {
     else if (v2IsZero) {return 0.0}
     else {
         var multiples: [Float] = Array(repeating: 0.0, count: v1.count)
-        for i in 0...v1.count{
+        for i in 0...v1.count-1{
             multiples[i] = v1[i] * v2[i]
         }
         var v1Norm: Float {
             var norm: Float = 0.0
-            for i in 0...v1.count{
+            for i in 0...v1.count-1{
                 norm = norm+sqrt(v1[i]*v1[i])
             }
             return Float(norm)
         }
         var v2Norm: Float {
             var norm:Float = 0.0
-            for i in 0...v2.count{
+            for i in 0...v2.count-1{
                 norm = norm+sqrt(v2[i]*v2[i])
             }
             return Float(norm)
@@ -70,7 +77,7 @@ func cosine_similarity(v1: [Float], v2: [Float]) -> Float {
 func euclidean_similarity(v1: [Float],v2: [Float]) -> Float {
     var d: Float {
         var ret: Float = 0.0
-        for i in 0...v1.count{
+        for i in 0...v1.count-1{
             ret = ret+sqrt((v2[i]-v1[i])*(v2[i]-v1[i]))
         }
         return ret
@@ -88,7 +95,66 @@ func scoreFunction(v1: [Float],v2: [Float])-> Float{
     else {return Float(penalty)}
 }
 
+/* Weird function
+func scoreFunctionSimple(v1: [Float],v2: [Float]){
+    try {
+        if (all(v1: v1 ,v2: v2)) {return 2}
+        return -1
+    }
+    exception {
+    }
+}
+*/
 
+class OnlineAlignment {
+    var n: Int
+    var prevRow: [Float]
+    var maxima: [Float] = []
+    var maximaPositions: [Float] = []
+    var positions: [Float] = []
+    var refFeatures:[Float]
+    var gapScore: Float
+    
+    init(refFeatures: [Float], gapScore: Float = -1) {
+        reset()
+        self.refFeatures = refFeatures
+        self.gapScore = gapScore
+        n = refFeatures.count
+        prevRow = Array(repeating: 0.0, count: n+1)
+        
+    }
+    
+    func align(v: [Float]) -> Float {
+        var nextRow: [Float] = Array(repeating: 0.0, count: n+1)
+        var match: Float
+        var delete: Float
+        var insert: Float
+        var maxPos: [Float] = []
+        var maxValue: Float
+        var nextPos : Float
+        for i in 1...(n+1){
+            match = prevRow[i-1] + scoreFunction(v1: v, v2: refFeatures) // Alex code has refFeatures[i-1]. Why?
+            delete = prevRow[i] + gapScore
+            insert = nextRow[i-1] + gapScore
+            
+            nextRow[i] = max(0.0, match, delete, insert)
+        }
+        maxValue = nextRow.max()!
+        maxima.append(nextRow.max()!)
+        for i in 0...nextRow.count-1{
+            if (nextRow[i]==maxValue){
+                maxPos.append(Float(i))
+                self.maximaPositions.append(Float(i))    // What is the difference between maxPos and maximaPositions?
+            }
+        }
+        
+        nextPos = maxPos[0] - 1 //Smith-Waterman Index -> Position
+        self.positions.append(nextPos)
+        
+        prevRow = nextRow
+        return nextPos
+    }
+}
 
 
 
